184e291c38f1c697963227ff8fb3b18d21555f84
meghagarg93 <108915561+meghagarg93@users.noreply.github.com>
2023-01-24 12:35:38 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,32 @@
+const superagent = require('superagent');
+superagent
+  .post('http://dls-cup-alpha-2093819659.us-west-2.elb.amazonaws.com/lb-auth/auth/cup_DAuZNjREhwNF5M6_jXwuh/ext-users/token')
+  .send({ 'ext_user_id': '5dfd3e079299405abcf04070c0396f2f' }) // sends a JSON post body
+  .set('Content-Type', 'application/json')
+  .end((err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Post response = ", res.status)
+      console.log(res.body);
+      accessToken = res.body.access_token;
+      console.log("access token : " + accessToken);
+    }
+  });
+
+superagent
+  .get("http://dls-cup-alpha-2093819659.us-west-2.elb.amazonaws.com/lb-auth/accounts/cup1/ext-users/5dfd3e079299405abcf04070c0396f2f/spaces")
+  .end((err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Get response = ", res.status)
+      console.log(res.body);
+    }
+  })
+
+
+
+
+
+
